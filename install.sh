844d9cf1c1ac892826fd3bd02d5d1947060b2928
Eric Kevrel <0x2E757@gmail.com>
2024-06-09 21:20:30 +0300

    Linux install script fixes and improvements

--- install.sh
+++ install.sh
@@ -1,43 +1,77 @@
 #!/bin/bash
 
 # 1. Check for necessary tools
-if ! command --version git >/dev/null; then
-  echo >&2 "Error: git is not installed or not accessible via \"git\" command (PATH missing?)."
-  exit 1
+if ! command -v git >/dev/null; then
+    echo >&2 "Error: git is not installed or not accessible via \"git\" command (PATH missing?)."
+    exit 1
 fi
 
-if ! command --version deno >/dev/null; then
-  echo >&2 "Error: Deno is not installed or not accessible via \"deno\" command (PATH missing?)."
-  exit 1
+if ! command -v deno >/dev/null; then
+    echo >&2 "Error: Deno is not installed or not accessible via \"deno\" command (PATH missing?)."
+    exit 1
 fi
 
-if ! command --version systemctl >/dev/null; then
-  echo >&2 "Error: systemctl not found, this script is intended for systems with systemd."
-  exit 1
+if ! command -v systemctl >/dev/null; then
+    echo >&2 "Error: systemctl not found, this script is intended for systems with systemd."
+    exit 1
 fi
 
 # 2. Prompt user for input
 DEFAULT_SERVICE_NAME="telegram-chatgpt-bot"
 read -p "Enter the service name [${DEFAULT_SERVICE_NAME}]: " SERVICE_NAME
-read -p "Enter your OpenAI API key (TCGB_API_KEY): " TCGB_API_KEY
-read -p "Enter your Telegram bot token (TCGB_TOKEN): " TCGB_TOKEN
-read -p "Enter allowed users (TCGB_ALLOWED_USERS): " TCGB_ALLOWED_USERS
 SERVICE_NAME=${SERVICE_NAME:-$DEFAULT_SERVICE_NAME}
+ENV_FILE="/etc/${SERVICE_NAME}.env"
+
+if [ -f "$ENV_FILE" ]; then
+    # 2.1 Prompt for ovewriting existing env values
+    export $(grep -v '^#' $ENV_FILE | xargs)
+    read -p "Enter your OpenAI API key [${TCGB_API_KEY}]: " NEW_TCGB_API_KEY
+    read -p "Enter your Telegram bot token [${TCGB_TOKEN}]: " NEW_TCGB_TOKEN
+    read -p "Enter allowed users [${TCGB_ALLOWED_USERS}]: " NEW_TCGB_ALLOWED_USERS
+    TCGB_API_KEY=${input_TCGB_API_KEY:-$TCGB_API_KEY}
+    TCGB_TOKEN=${input_TCGB_TOKEN:-$TCGB_TOKEN}
+    TCGB_ALLOWED_USERS=${input_TCGB_ALLOWED_USERS:-$TCGB_ALLOWED_USERS}
+else
+    # 2.1 Prompt for input env values
+    read -p "Enter your OpenAI API key: " TCGB_API_KEY
+    read -p "Enter your Telegram bot token: " TCGB_TOKEN
+    read -p "Enter allowed users: " TCGB_ALLOWED_USERS
+fi
+
 
 # 3. Clone the repository
 REPO_URL="https://github.com/0x2E757/Telegram-ChatGPT-bot"
 DAEMON_DIR="/usr/local/lib/${SERVICE_NAME}"
-sudo git clone --depth 1 $REPO_URL $DAEMON_DIR
+
+if [ -d "$DAEMON_DIR" ]; then
+    read -p "Directory $DAEMON_DIR already exists. Do you want to clone latest changes? [Y/n] " CLONE_REQUIRED
+    case "$CLONE_REQUIRED" in
+        [Nn]* ) 
+            echo "Clone skipped."
+            ;;
+        * ) 
+            echo "Deleting $DAEMON_DIR"
+            sudo rm -rf "$DAEMON_DIR"
+            echo "Cloning $REPO_URL (without history)"
+            sudo git clone --depth 1 $REPO_URL $DAEMON_DIR
+        ;;
+    esac
+else
+    echo "Cloning $REPO_URL (without history)"
+    sudo git clone --depth 1 $REPO_URL $DAEMON_DIR
+fi
 
 # 4. Create the environment variables file
-ENV_FILE="/etc/${SERVICE_NAME}.env"
-echo "TCGB_API_KEY=${TCGB_API_KEY}" | sudo tee $ENV_FILE
-echo "TCGB_TOKEN=${TCGB_TOKEN}" | sudo tee -a $ENV_FILE
-echo "TCGB_ALLOWED_USERS=${TCGB_ALLOWED_USERS}" | sudo tee -a $ENV_FILE
+sudo tee $ENV_FILE > /dev/null <<EOF
+TCGB_API_KEY=${TCGB_API_KEY}
+TCGB_TOKEN=${TCGB_TOKEN}
+TCGB_ALLOWED_USERS=${TCGB_ALLOWED_USERS}
+EOF
 
 # 5. Create the systemd service file
+DENO_BIN=$(which deno)
 SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}.service"
-sudo bash -c "cat > $SERVICE_FILE <<EOL
+sudo tee $SERVICE_FILE > /dev/null <<EOF
 [Unit]
 Description=Telegram ChatGPT bot daemon (${SERVICE_NAME})
 After=network.target
@@ -46,12 +80,12 @@ Wants=network.target
 [Service]
 EnvironmentFile=${ENV_FILE}
 WorkingDirectory=${DAEMON_DIR}/src
-ExecStart=deno run --allow-all main.ts
+ExecStart=${DENO_BIN} run --allow-all main.ts
 Restart=always
 
 [Install]
 WantedBy=multi-user.target
-EOL"
+EOF
 
 # 6. Reload systemd, enable and start the service
 sudo systemctl daemon-reload
